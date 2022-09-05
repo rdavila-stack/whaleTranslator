9df12bac559d0e34dc3aae316fc3272ea45004bb
Ramon Davila <78738387+rdavila-stack@users.noreply.github.com>
2022-09-05 22:25:09 +0200

    Add files via upload
    A simple program that uses nested for loops to code input into a vowel based 'language' that whales supposedly can understand.

---
+++ whaleTranslator.js
@@ -0,0 +1,19 @@
+let input = "I love Peppa Pig!";
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+}
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
